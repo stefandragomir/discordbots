aade59bd61f2c8abae95285c6e736c9d85370f7b
stefan.dragomir <stefan.dragomir.000@gmail.com>
2021-10-13 21:29:09 +0300

    added new didi implementation

--- didi/didi.py
+++ didi/didi.py
@@ -2,58 +2,97 @@
 import sys
 import os
 import discord
-import asyncio
-import furion_msg
 
-from furion_cst    import *
-from furion_msg    import Furion_Message
-from furion_logger import Furion_Logger
+from didi_msg      import DD_Message
+from didi_logger   import DD_Logger
+from didi_config   import DD_Config
 
-'''********************************************************************************************************
-***********************************************************************************************************
-********************************************************************************************************'''
-log         = Furion_Logger()
 
 '''********************************************************************************************************
 ***********************************************************************************************************
 ********************************************************************************************************'''
-client = discord.Client()
+class DD():
+
+    def __init__(self):
+
+        self.config    = DD_Config()
+        self.client    = discord.Client()
+        self.log       = DD_Logger()
+        self.guild     = None
+        self.channel   = None
+
+    def connect(self):
+
+        self.get_guild()
+
+        self.get_channel()
+
+        if self.guild != None and self.channel != None:
+
+            self.log.info("didi connected tto guild [%s] channel [%s]" % (self.guild.name,self.channel))
+
+        else:
+            self.log.info("didi could not connect to guild [%s] channel [%s]" % (self.guild.name,self.channel))
+
+    def disconnect(self):
+
+        self.log.info("didi disconnected from guild [%s] channel [%s]" % (self.guild.name,self.channel))
+
+    def run(self):
+
+        self.client.run(self.config.bot_token)
+
+    def get_guild(self):
+
+        for _guild in self.client.guilds:
+
+            if _guild.name == self.config.guild_name:
+
+                self.guild = _guild
+
+    def get_channel(self):
+
+        for _channel in self.guild.text_channels:
+
+            if _channel.name == self.config.channel_name:
+
+                self.channel = _channel
+
+    async def on_message(self,message):
+
+        DD_Message(self,message).reply()
 
 '''********************************************************************************************************
 ***********************************************************************************************************
 ********************************************************************************************************'''
-@client.event
+
+didi = DD()
+
+'''********************************************************************************************************
+***********************************************************************************************************
+********************************************************************************************************'''
+@didi.client.event
 async def on_ready():
 
-    global guild
-
-    for _guild in client.guilds:
-
-        if _guild.name == FURION_GUILD:
-
-            guild = _guild
-
-            log.info("furion connected to %s" % (_guild.name,))
+    didi.connect()
 
 '''********************************************************************************************************
 ***********************************************************************************************************
 ********************************************************************************************************'''
-@client.event
+@didi.client.event
 async def on_disconnect():
 
-    log.info("furion disconnected")
+    didi.disconnect()
 
 '''********************************************************************************************************
 ***********************************************************************************************************
 ********************************************************************************************************'''
-@client.event
+@didi.client.event
 async def on_message(message):
 
-    _msg = Furion_Message(client, message)
-
-    _msg.reply()
+    didi.on_message(message)
 
 '''********************************************************************************************************
 ***********************************************************************************************************
 ********************************************************************************************************'''
-client.run(FURION_TOKEN)
+didi.run()
